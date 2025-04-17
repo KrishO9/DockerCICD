832cccccb709a39c6e940c0bbfa0de5bc70e8cf7
KrishO9 <krish27n@gmail.com>
2025-04-17 20:49:33 +0530

    cicd.yml file

--- test_app.py
+++ test_app.py
@@ -1,7 +1,11 @@
 from app import app
 
+#pytest library finds out the testing files
+# By indentifying files and functions starting with test
+
 def test_home():
     response=app.test_client().get("/")
 
     assert response.status_code==200
-    assert response.data== b"Hello World!"
+    assert response.data=="Hello World"
+
